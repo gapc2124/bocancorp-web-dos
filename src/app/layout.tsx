655db7e5de94dd97c6d65649048e7cd925d777ab
GabrielP <gapc1418@gmail.com>
2026-07-31 22:07:40 -0500

    feat: add Apollo tracking script to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -13,6 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="apollo-tracker" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"6a4826e95dc1dc0018c57e26"})},
+document.head.appendChild(o)}initApollo();`
+        }} />
+      </head>
       <body className="antialiased bg-[#000c2d] text-white overflow-x-clip min-h-screen">
         {children}
       </body>
